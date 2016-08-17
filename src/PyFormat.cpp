28fa6fea34a8790b1d2ca69b6268607a7d2c5fe7
Pavel Schon <pavel@schon.cz>
2016-08-17 21:39:41 +0200

    templateize creation of Format

--- src/PyFormat.cpp
+++ src/PyFormat.cpp
@@ -8,9 +8,6 @@
 #include <boost/python.hpp>
 #include <boost/format.hpp>
 
-typedef boost::format::string_type  string_type;
-typedef boost::wformat::string_type wstring_type;
-
 
 namespace boost
 {
@@ -18,21 +15,37 @@ namespace boost
 namespace python
 {
 
+typedef boost::format::string_type  string;
+typedef boost::wformat::string_type wstring;
+
 /**
  * @brief Convert boost::python::object to unicode
  *
  */
 std::wostream& operator<<( std::wostream& out, const object& obj )
 {
-    const wstring_type s = extract<wstring_type>( str( obj ) );
+    const wstring s = extract<wstring>( str( obj ) );
 
     return out << s;
 }
 
 #if PY_MAJOR_VERSION >= 3
 
-const auto builtins = import("builtins");
-const auto bytes    = builtins.attr("bytes");
+const auto encoding = "utf-8";
+const auto builtins = import( "builtins" );
+const auto bytes    = builtins.attr( "bytes" );
+
+
+/**
+ * @brief Convert unicode to bytes
+ *
+ */
+template<class FORMAT>
+object fmtToBytes( const FORMAT& fmt )
+{
+    return bytes( fmt.str(), encoding );
+}
+
 
 /**
  * @brief Convert boost::python::object to unicode
@@ -40,18 +53,9 @@ const auto bytes    = builtins.attr("bytes");
  */
 std::ostream& operator<<( std::ostream& out, const object& obj )
 {
-    const string_type s = extract<string_type>( bytes( str( obj ), "utf-8" ) );
+    const string s = extract<string>( bytes( str( obj ), encoding ) );
 
     return out << s;
-}
-
-/**
- * @brief Convert unicode to bytes
- *
- */
-object formatToBytes( const boost::format& fmt )
-{
-    return bytes( fmt.str(), "utf-8" );
 }
 
 #else
@@ -62,80 +66,74 @@ object formatToBytes( const boost::format& fmt )
  */
 std::ostream& operator<<( std::ostream& out, const object& obj )
 {
-    const string_type s = extract<string_type>( str( obj ) );
+    const string s = extract<string>( str( obj ) );
 
     return out << s;
 }
 
 #endif
 
+/**
+ * @brief Expose Format class
+ *
+ */
+template<class FORMAT>
+void expose( const FORMAT& fmt, const char* const name )
+{
+    typedef typename FORMAT::string_type string_t;
 
-} /* namespace boost */
+    const return_internal_reference<> return_policy;
+
+    class_< FORMAT >( name, init<const string_t&>() )
+#if PY_MAJOR_VERSION >= 3
+        .def( "__bytes__",      &fmtToBytes<FORMAT> )
+        .def( "__str__",        &FORMAT::str )
+#else
+        .def( "__str__",        &FORMAT::str )
+        .def( "__unicode__",    &FORMAT::str )
+#endif
+        .def( "__mod__",        &FORMAT::template operator% <const object&>,    return_policy  )
+        .def( "__mod__",        &FORMAT::template operator% <const string_t&>,  return_policy  )
+        .def( "__mod__",        &FORMAT::template operator% <const int&>,       return_policy  )
+        .def( "__mod__",        &FORMAT::template operator% <const long&>,      return_policy  )
+        .def( "__mod__",        &FORMAT::template operator% <const long long&>, return_policy  )
+        .def( "__mod__",        &FORMAT::template operator% <const float&>,     return_policy  )
+        .def( "__mod__",        &FORMAT::template operator% <const double&>,    return_policy  )
+
+        .def( "swap",           &FORMAT::swap,          return_policy  )
+        .def( "clear",          &FORMAT::clear,         return_policy  )
+        .def( "clear_binds",    &FORMAT::clear_bind,    return_policy  )
+        .def( "parse",          &FORMAT::parse,         return_policy  )
+
+        /* const functions */
+        .def( "size",           &FORMAT::size           )
+        .def( "expected_args",  &FORMAT::expected_args  )
+        .def( "bound_args",     &FORMAT::bound_args     )
+        .def( "fed_args",       &FORMAT::fed_args       )
+        .def( "remaining_args", &FORMAT::remaining_args )
+    ;
+}
+
 
 } /* namespace python */
+
+} /* namespace boost */
 
 
 BOOST_PYTHON_MODULE( pyformat )
 {
     using namespace boost::python;
+    using boost::io::format_error;
 
-    register_exception_translator<boost::io::format_error>( []( const boost::io::format_error& e )
+    register_exception_translator<format_error>( []( const format_error& e )
     {
         PyErr_SetString( PyExc_ValueError, e.what() );
 
         throw_error_already_set();
     });
 
-    const return_internal_reference<> return_policy;
-
-    class_< boost::wformat >( "UFormat", init<const wstring_type&>()                            )
-#if PY_MAJOR_VERSION >= 3
-        .def( "__str__",        &boost::wformat::str                                            )
-#else
-        .def( "__unicode__",    &boost::wformat::str                                            )
-#endif
-        .def( "__mod__",        &boost::wformat::operator% <const object&>,       return_policy )
-        .def( "__mod__",        &boost::wformat::operator% <const wstring_type&>, return_policy )
-        .def( "__mod__",        &boost::wformat::operator% <const int&>,          return_policy )
-        .def( "__mod__",        &boost::wformat::operator% <const long&>,         return_policy )
-        .def( "__mod__",        &boost::wformat::operator% <const long long&>,    return_policy )
-        .def( "__mod__",        &boost::wformat::operator% <const float&>,        return_policy )
-        .def( "__mod__",        &boost::wformat::operator% <const double&>,       return_policy )
-        .def( "swap",           &boost::wformat::swap,                            return_policy )
-        .def( "clear",          &boost::wformat::clear,                           return_policy )
-        .def( "clear_binds",    &boost::wformat::clear_bind,                      return_policy )
-        .def( "parse",          &boost::wformat::parse,                           return_policy )
-        .def( "size",           &boost::wformat::size                                           )
-        .def( "expected_args",  &boost::wformat::expected_args                                  )
-        .def( "bound_args",     &boost::wformat::bound_args                                     )
-        .def( "fed_args",       &boost::wformat::fed_args                                       )
-        .def( "remaining_args", &boost::wformat::remaining_args                                 )
-    ;
-
-    class_< boost::format >( "Format", init<const string_type&>()                               )
-#if PY_MAJOR_VERSION >= 3
-        .def( "__bytes__",      &formatToBytes                                                  )
-    #else
-        .def( "__str__",        &boost::format::str                                             )
-    #endif
-        .def( "__mod__",        &boost::format::operator% <const object&>,       return_policy  )
-        .def( "__mod__",        &boost::format::operator% <const string_type&>,  return_policy  )
-        .def( "__mod__",        &boost::format::operator% <const int&>,          return_policy  )
-        .def( "__mod__",        &boost::format::operator% <const long&>,         return_policy  )
-        .def( "__mod__",        &boost::format::operator% <const long long&>,    return_policy  )
-        .def( "__mod__",        &boost::format::operator% <const float&>,        return_policy  )
-        .def( "__mod__",        &boost::format::operator% <const double&>,       return_policy  )
-        .def( "swap",           &boost::format::swap,                            return_policy  )
-        .def( "clear",          &boost::format::clear,                           return_policy  )
-        .def( "clear_binds",    &boost::format::clear_bind,                      return_policy  )
-        .def( "parse",          &boost::format::parse,                           return_policy  )
-        .def( "size",           &boost::format::size                                            )
-        .def( "expected_args",  &boost::format::expected_args                                   )
-        .def( "bound_args",     &boost::format::bound_args                                      )
-        .def( "fed_args",       &boost::format::fed_args                                        )
-        .def( "remaining_args", &boost::format::remaining_args                                  )
-        ;
-
+    expose( boost::format(),   "Format" );
+    expose( boost::wformat(), "UFormat" );
 
     auto module = scope();
 
