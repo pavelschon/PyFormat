f316d9a6a04cfe21aed244af8c44cd33158c49f6
Pavel Schon <pavel@schon.cz>
2016-08-18 22:03:37 +0200

    enhance unit test

--- tests/unit_test3.py
+++ tests/unit_test3.py
@@ -1,47 +1,101 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
+import sys
 import unittest
 
-from pyformat import UFormat as F
-from pyformat import Format  as S
+from pyformat import Format as F, UFormat as U
+
+
+if sys.version_info[0] >= 3:
+    str_, bytes_ = str, bytes
+else:
+    str_, bytes_ = unicode, str
 
 
 class TestUFormat(unittest.TestCase):
     def testInit(self):
-        f = F( '' )
-        f = F( '%|| %|| %||' )
-        f = F( '%1% %2% %3%' )
+        U()
+        U( '' )
+        U( '%|| %|| %||' )
+        U( '%1% %2% %3%' )
 
         with self.assertRaises(ValueError):
-            f = F( '%|xx|')
+            U( '%|xx|')
 
-        s = S( b'' )
-        s = S( b'%|| %|| %||' )
-        s = S( b'%1% %2% %3%' )
+        F()
+        F( b'' )
+        F( b'%|| %|| %||' )
+        F( b'%1% %2% %3%' )
 
         with self.assertRaises(ValueError):
-            s = S( '%|xx|')
+            F( b'%|xx|')
 
 
     def testOutput(self):
-        f = F('%|| %|| %||')
-        self.assertEqual(str( f % 'ě' % 'č' % 'š' ), 'ě č š')
+        u = U('%|| %|| %||')
+        self.assertEqual(str_( u % 'ě' % 'č' % 'š' ), 'ě č š')
+        self.assertEqual(bytes_( u ), b'\xc4\x9b \xc4\x8d \xc5\xa1')
 
-        f = F('%|.2f|')
-        self.assertEqual(str( f % 1.0 ), '1.00')
+        u = U('%|.2f|')
+        self.assertEqual(str_( u % 1.0 ), '1.00')
 
-        f = F('%||')
-        self.assertEqual(str( f % None ), 'None')
+        u = U('%||')
+        self.assertEqual(str_( u % None ), 'None')
+        self.assertEqual(bytes_( u % None ), b'None')
 
-        s = S(b'%|| %|| %||')
-        self.assertEqual(bytes( s % b'e' % b'c' % b's' ), b'e c s')
+        u = U('%||')
+        self.assertEqual(bytes_( u % None ), b'None')
+        self.assertEqual(bytes_( u % None ), b'None')
 
-        s = S(b'%|.2f|')
-        self.assertEqual(bytes( s % 1.0 ), b'1.00')
+        f = F(b'%|| %|| %||')
+        self.assertEqual(bytes_( f % b'e' % b'c' % b's' ), b'e c s')
 
-        s = S('%||')
-        self.assertEqual(bytes( s % None ), b'None')
+        f = F(b'%|| %|| %||')
+        self.assertEqual(bytes_( f % b'e' % b'c' % b's' ), b'e c s')
+        self.assertEqual(str_( f % 'ě' % 'č' % 'š' ), 'ě č š')
+
+        f = F(b'%|.2f|')
+        self.assertEqual(bytes_( f % 1.0 ), b'1.00')
+
+        f = F(b'%||')
+        self.assertEqual(bytes_( f % None ), b'None')
+
+
+    def testTooMany(self):
+        f = F(b'%||')
+        with self.assertRaises(ValueError):
+            f % 1 % 2
+
+        u = U('%||')
+        with self.assertRaises(ValueError):
+            u % 1 % 2
+
+
+    def testTooFew(self):
+        f = F(b'%||%||')
+        with self.assertRaises(ValueError):
+            bytes_(f % 1)
+
+        u = U('%||%||')
+        with self.assertRaises(ValueError):
+            str_(u % 1)
+
+
+    def testSwap(self):
+        f, g = F(b'f'), F(b'g')
+        f.swap(g)
+
+        self.assertEqual(bytes_( f ), b'g')
+        self.assertEqual(bytes_( g ), b'f')
+
+        u, v = U('u'), U(b'v')
+        u.swap(v)
+
+        self.assertEqual(str_( u ), 'v')
+        self.assertEqual(str_( v ), 'u')
 
 
 if __name__ == '__main__':
